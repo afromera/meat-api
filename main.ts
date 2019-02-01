c5956ec16c61155062beecb025a5bf218a7c2f25
Alex Fernandes Romera <af_romera@hotmail.com>
2019-01-31 23:58:59 -0200

    restify: configurado rota hello e a porta 3000

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-console.log("main script 2...")
+
+import * as restify from 'restify'
+
+const server = restify.createServer({
+    name: "meat-api",
+    version: "1.0.0"
+})
+
+//registrar a rota hello
+server.get('/hello', (req,resp,next) => {
+    resp.json({message: "Hello"})
+    next()
+})
+
+//configurar a porta
+server.listen(3000, ()=> {
+    console.log('API is running on http://localhost:3000')
+})
+
