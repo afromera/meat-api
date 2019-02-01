bbb41e670e9febf01156bce5f772acc31c83b3d8
Alex Fernandes Romera <af_romera@hotmail.com>
2019-02-01 00:45:34 -0200

    request, response e next

--- dist/main.js
+++ dist/main.js
@@ -5,11 +5,43 @@ const server = restify.createServer({
     name: "meat-api",
     version: "1.0.0"
 });
-//registrar a rota hello
-server.get('/hello', (req, resp, next) => {
-    resp.json({ message: "Hello" });
-    next();
-});
+//para restify conseguir obter o resultado dos parametros passado na querystring
+server.use(restify.plugins.queryParser());
+//registrar a rota info
+server.get('/info', [
+    (req, resp, next) => {
+        //barrando o IE7
+        if (req.userAgent() && req.userAgent().includes('MSIE 7.0')) {
+            ////pode utilizar assim
+            //resp.status(400)
+            //resp.json({ message: "Favor Atualizar ou utilizar outro Navegador" })
+            ////vai barrar a proxima chamada
+            //return next(false)
+            //ou pode criar objeto de erro
+            let error = new Error();
+            error.statusCode = 400;
+            error.message = "Favor Atualizar ou utilizar outro Navegador";
+            return next(error);
+        }
+        //restify: sempre utilizar quando finalizar ou chamar a proxima requisicao
+        return next();
+    },
+    (req, resp, next) => {
+        //resp.contentType = "application/json";
+        //resp.status(400)
+        //resp.setHeader('Content-Type', 'application/json')    
+        //resp.send({message:"Hello"})
+        resp.json({
+            browser: req.userAgent(),
+            method: req.method,
+            url: req.href(),
+            path: req.path(),
+            query: req.query
+        });
+        // aqui estou somente finalizando
+        return next();
+    }
+]);
 //configurar a porta
 server.listen(3000, () => {
     console.log('API is running on http://localhost:3000');
