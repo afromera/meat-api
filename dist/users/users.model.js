56b869e699b24b0ccf75163f3ef52312ca96990a
Alex Fernandes Romera <af_romera@hotmail.com>
2019-02-02 17:38:53 -0200

    Configurando rota para retornar apenas um usuario

--- dist/users/users.model.js
+++ dist/users/users.model.js
@@ -1,13 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const users = [
-    { name: 'Alex Fernandes Romera', email: 'af_romera@hotmail.com' },
-    { name: 'Grazielle Allonso Romera', email: 'grazi@gmail.com' },
-    { name: 'Caroline Allonso', email: 'carol@gmail.com' }
+    { id: '1', name: 'Alex Fernandes Romera', email: 'af_romera@hotmail.com' },
+    { id: '2', name: 'Grazielle Allonso Romera', email: 'grazi@gmail.com' },
+    { id: '3', name: 'Caroline Allonso', email: 'carol@gmail.com' }
 ];
 class User {
     static findAll() {
         return Promise.resolve(users);
+    }
+    static findById(id) {
+        let filtered = users.filter(user => user.id === id);
+        let user = undefined;
+        if (filtered.length > 0) {
+            user = filtered[0];
+        }
+        return Promise.resolve(user);
     }
 }
 exports.User = User;
--- users/users.model.ts
+++ users/users.model.ts
@@ -1,12 +1,25 @@
+import { promises } from "fs";
 
 const users = [
-    {name: 'Alex Fernandes Romera', email: 'af_romera@hotmail.com'}
-    ,{name: 'Grazielle Allonso Romera', email: 'grazi@gmail.com'}
-    ,{name: 'Caroline Allonso', email: 'carol@gmail.com'}
+    {id: '1', name: 'Alex Fernandes Romera', email: 'af_romera@hotmail.com'}
+    ,{id: '2', name: 'Grazielle Allonso Romera', email: 'grazi@gmail.com'}
+    ,{id: '3', name: 'Caroline Allonso', email: 'carol@gmail.com'}
 ]
 
 export class User {
     static findAll(): Promise<any[]>{
         return Promise.resolve(users)
+    }
+
+    static findById(id: string): Promise<any>{
+
+        let filtered = users.filter(user => user.id === id)
+        let user = undefined
+
+        if(filtered.length > 0){
+            user = filtered[0]
+        }
+
+        return Promise.resolve(user);
     }
 }
