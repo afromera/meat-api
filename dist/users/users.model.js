d7e90e5f06abd67a4a8a90212324694409601736
Alex Fernandes Romera <af_romera@hotmail.com>
2019-02-05 01:08:31 -0200

    modificando model para utilizar mongodb e mongoose

--- dist/users/users.model.js
+++ dist/users/users.model.js
@@ -1,21 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const users = [
-    { id: '1', name: 'Alex Fernandes Romera', email: 'af_romera@hotmail.com' },
-    { id: '2', name: 'Grazielle Allonso Romera', email: 'grazi@gmail.com' },
-    { id: '3', name: 'Caroline Allonso', email: 'carol@gmail.com' }
-];
-class User {
-    static findAll() {
-        return Promise.resolve(users);
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String,
+        select: false
     }
-    static findById(id) {
-        let filtered = users.filter(user => user.id === id);
-        let user = undefined;
-        if (filtered.length > 0) {
-            user = filtered[0];
-        }
-        return Promise.resolve(user);
-    }
-}
-exports.User = User;
+});
+exports.User = mongoose.model('User', userSchema);
--- users/users.model.ts
+++ users/users.model.ts
@@ -1,25 +1,17 @@
-import { promises } from "fs";
+import * as mongoose from 'mongoose'
 
-const users = [
-    {id: '1', name: 'Alex Fernandes Romera', email: 'af_romera@hotmail.com'}
-    ,{id: '2', name: 'Grazielle Allonso Romera', email: 'grazi@gmail.com'}
-    ,{id: '3', name: 'Caroline Allonso', email: 'carol@gmail.com'}
-]
-
-export class User {
-    static findAll(): Promise<any[]>{
-        return Promise.resolve(users)
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String,
+        select: false
     }
+})
 
-    static findById(id: string): Promise<any>{
-
-        let filtered = users.filter(user => user.id === id)
-        let user = undefined
-
-        if(filtered.length > 0){
-            user = filtered[0]
-        }
-
-        return Promise.resolve(user);
-    }
-}
+export const User = mongoose.model('User', userSchema)
