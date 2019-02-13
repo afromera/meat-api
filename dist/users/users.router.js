07da707139ce8e0adce0efcf89419923eefc7873
Alex Fernandes Romera <af_romera@hotmail.com>
2019-02-12 22:35:49 -0200

    refatorando users router para tratar erros

--- dist/users/users.router.js
+++ dist/users/users.router.js
@@ -11,20 +11,27 @@ class UsersRouter extends router_1.Router {
     }
     applyRoutes(application) {
         application.get('/users', (req, resp, next) => {
-            users_model_1.User.find().then(this.render(resp, next));
+            users_model_1.User.find()
+                .then(this.render(resp, next))
+                .catch(next);
         });
         application.get('users/:id', (req, resp, next) => {
-            users_model_1.User.findById(req.params.id).then(this.render(resp, next));
+            users_model_1.User.findById(req.params.id)
+                .then(this.render(resp, next))
+                .catch(next);
         });
         //verbo post é utilizado para incluir um novo registro
         application.post('users/', (req, resp, next) => {
             let user = new users_model_1.User(req.body);
-            user.save().then(this.render(resp, next));
+            user.save()
+                .then(this.render(resp, next))
+                .catch(next);
         });
         //verbo put é utilizado para atualizar TODOS os campos de um registro
         application.put('users/:id', (req, resp, next) => {
             const options = { overwrite: true };
-            users_model_1.User.update({ _id: req.params.id }, req.body, options).exec()
+            users_model_1.User.update({ _id: req.params.id }, req.body, options)
+                .exec()
                 .then(result => {
                 if (result.n) {
                     return users_model_1.User.findById(req.params.id);
@@ -32,24 +39,24 @@ class UsersRouter extends router_1.Router {
                 else {
                     resp.send(404);
                 }
-            }).then(this.render(resp, next));
+            }).then(this.render(resp, next))
+                .catch(next);
         });
         //verbo patch é utilizando para atualizar apenas os campos informados do registro
         application.patch('users/:id', (req, resp, next) => {
             const options = { new: true };
-            users_model_1.User.findByIdAndUpdate(req.params.id, req.body, options).then(this.render(resp, next));
+            users_model_1.User.findByIdAndUpdate(req.params.id, req.body, options)
+                .then(this.render(resp, next))
+                .catch(next);
         });
         //verbo delete é utilizado para remover o registro
         application.del('users/:id', (req, resp, next) => {
-            users_model_1.User.remove({ _id: req.params.id }).exec().then((cmdResult) => {
-                if (cmdResult.result.n) {
-                    resp.send(204);
-                }
-                else {
-                    resp.send(404);
-                }
+            users_model_1.User.remove({ _id: req.params.id })
+                .exec()
+                .then((cmdResult) => {
+                (cmdResult.result.n) ? resp.send(204) : resp.send(404);
                 return next();
-            });
+            }).catch(next);
         });
     }
 }
--- users/users.router.ts
+++ users/users.router.ts
@@ -4,7 +4,7 @@ import { User } from './users.model'
 
 class UsersRouter extends Router {
 
-    constructor(){
+    constructor() {
         super()
         this.on('beforeRender', document => {
             document.password = undefined
@@ -14,50 +14,58 @@ class UsersRouter extends Router {
     applyRoutes(application: restify.Server) {
 
         application.get('/users', (req, resp, next) => {
-            User.find().then(this.render(resp, next))
+            User.find()
+                .then(this.render(resp, next))
+                .catch(next)
         })
 
         application.get('users/:id', (req, resp, next) => {
-            User.findById(req.params.id).then(this.render(resp, next))
+            User.findById(req.params.id)
+                .then(this.render(resp, next))
+                .catch(next)
         })
 
         //verbo post é utilizado para incluir um novo registro
         application.post('users/', (req, resp, next) => {
             let user = new User(req.body)
-            user.save().then(this.render(resp,next))
+            user.save()
+                .then(this.render(resp, next))
+                .catch(next)
         })
 
         //verbo put é utilizado para atualizar TODOS os campos de um registro
         application.put('users/:id', (req, resp, next) => {
             const options = { overwrite: true }
 
-            User.update({ _id: req.params.id }, req.body, options).exec()
+            User.update({ _id: req.params.id }, req.body, options)
+                .exec()
                 .then(result => {
                     if (result.n) {
                         return User.findById(req.params.id)
                     } else {
                         resp.send(404)
                     }
-                }).then(this.render(resp,next))
+                }).then(this.render(resp, next))
+                .catch(next)
         })
 
         //verbo patch é utilizando para atualizar apenas os campos informados do registro
         application.patch('users/:id', (req, resp, next) => {
             const options = { new: true }
-            User.findByIdAndUpdate(req.params.id, req.body, options).then(this.render(resp,next))
+            User.findByIdAndUpdate(req.params.id, req.body, options)
+                .then(this.render(resp, next))
+                .catch(next)
         })
 
         //verbo delete é utilizado para remover o registro
         application.del('users/:id', (req, resp, next) => {
 
-            User.remove({ _id: req.params.id }).exec().then((cmdResult: any) => {
-                if (cmdResult.result.n) {
-                    resp.send(204)
-                } else {
-                    resp.send(404)
-                }
-                return next()
-            })
+            User.remove({ _id: req.params.id })
+                .exec()
+                .then((cmdResult: any) => {
+                    (cmdResult.result.n) ? resp.send(204) : resp.send(404)
+                    return next()
+                }).catch(next)
 
         })
 
